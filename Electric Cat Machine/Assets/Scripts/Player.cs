5269a808ade74e19904d8b4ba34e7b61e81ec34e
Bradley Shively <bshively@gmail.com>
2017-04-22 21:01:39 -0400

    Implement attack and refactor movement controls

--- Electric Cat Machine/Assets/Scripts/Player.cs
+++ Electric Cat Machine/Assets/Scripts/Player.cs
@@ -4,49 +4,92 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-	private Rigidbody2D myRigidBody;
-	private Animator myAnimator;
+    private Rigidbody2D myRigidBody;
+    private Animator myAnimator;
 
-	[SerializeField] // Shows the movement speed in the inspector
+    [SerializeField] // Shows the movement speed in the inspector
 	private float movementSpeed;
 
-	private bool facingRight;
+    private bool attack;
+    private bool facingRight;
 
-	// Use this for initialization
-	void Start()
-	{
-		facingRight = true;
-		myRigidBody = GetComponent<Rigidbody2D>();
-		myAnimator = GetComponent<Animator>();
-	}
+    // Use this to "freeze" the player, i.e. can't move when attacking
+    private bool canMove;
 
-	void FixedUpdate()
-	{
-		float horizontal = Input.GetAxis("Horizontal");
 
-		HandleMovement(horizontal);
-		Flip(horizontal);
-	}
+    // Use this for initialization
+    void Start()
+    {
+        canMove = true;
+        facingRight = true;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+    }
 
-	private void HandleMovement(float horizontal)
-	{
-		myRigidBody.velocity = new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y);
+    void FixedUpdate()
+    {
+        HandleInput();
+    }
 
-		// Set speed inside animator for controlling run animation
-		myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
-	}
+    private void HandleAttacks()
+    {
+        canMove = false;
+        myRigidBody.velocity = Vector2.zero;
+        myAnimator.SetTrigger("attack");
+    }
 
-	// Flip player sprite to face the correct direction based on movement
-	private void Flip(float horizontal)
-	{
-		if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
-		{
-			facingRight = !facingRight;
+    private void HandleInput()
+    {
+        // Don't process any input if the player is attacking
+        if (myAnimator.GetCurrentAnimatorStateInfo(0).IsTag("Attack"))
+        {
+            myRigidBody.velocity = Vector2.zero;
+        }
 
-			// Flip x scale, update Player's localScale
-			Vector3 scale = transform.localScale;
-			scale.x *= -1;
-			transform.localScale = scale;
-		}
-	}
+        // Proceed with normal input processing/movement
+        if (canMove)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                attack = true;
+                canMove = false;
+                myRigidBody.velocity = Vector2.zero;
+                HandleAttacks();
+            }
+            else
+            {
+                float horizontal = Input.GetAxis("Horizontal");
+                myRigidBody.velocity = new Vector2(horizontal * movementSpeed, myRigidBody.velocity.y);
+
+                // Set speed inside animator for controlling run animation
+                myAnimator.SetFloat("speed", Mathf.Abs(horizontal));
+
+                Flip(horizontal);
+            }
+
+        }
+    }
+
+    // Flip player sprite to face the correct direction based on movement
+    private void Flip(float horizontal)
+    {
+        if (canMove)
+        {
+            if (horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+            {
+                facingRight = !facingRight;
+
+                // Flip x scale, update Player's localScale
+                Vector3 scale = transform.localScale;
+                scale.x *= -1;
+                transform.localScale = scale;
+            }
+        }
+    }
+
+    private void ResetValues()
+    {
+        attack = false;
+        canMove = true;
+    }
 }
