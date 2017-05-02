d108f9050bf89000ade7c432930815e2520b3d4d
Bradley Shively <bshively@gmail.com>
2017-05-01 23:59:41 -0400

    Create laser pointer mechanic prototype and scene

---
+++ Electric Cat Machine/Assets/Scripts/Laser.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Laser : MonoBehaviour
+{
+    private LineRenderer lineRenderer;
+    private Vector3 laserPoint;
+    private Vector3 mouseWorld;
+    //    private
+    // Use this for initialization
+    void Start()
+    {
+        Cursor.visible = false;
+        lineRenderer = GetComponent<LineRenderer>();
+        lineRenderer.sortingLayerName = "Laser";
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+    }
+	
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            lineRenderer.enabled = true;
+            // Translate mouse position to world position
+            mouseWorld = Input.mousePosition;
+            mouseWorld.z = 10.0f;
+            mouseWorld = Camera.main.ScreenToWorldPoint(mouseWorld);
+//        Vector3[] lineCoordinates = [this.transform, Input.mousePosition];
+            if (Input.GetMouseButton(0))
+            {
+                lineRenderer.SetPositions(new Vector3[]{ this.transform.position, mouseWorld });
+            }
+        }
+        else
+        {
+            lineRenderer.enabled = false;
+        }
+    }
+
+}
---
+++ Electric Cat Machine/Assets/Scripts/LaserFollower.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserFollower : MonoBehaviour
+{
+    private Vector3 mouseWorld;
+    private Rigidbody2D rigidBody;
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+	
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        mouseWorld = Input.mousePosition;
+        mouseWorld.z = 10.0f;
+        mouseWorld = Camera.main.ScreenToWorldPoint(mouseWorld);
+        float xpos = this.transform.position.x;
+
+        if (Input.GetMouseButton(0))
+        {
+            if (mouseWorld.x > xpos)
+            {
+                Vector3 scale = transform.localScale;
+                scale.x = .3f;
+                transform.localScale = scale;
+                rigidBody.velocity = new Vector2(5, 0);
+            }
+            else if (mouseWorld.x < xpos)
+            {
+                Vector3 scale = transform.localScale;
+                scale.x = -.3f;
+                transform.localScale = scale;
+                rigidBody.velocity = new Vector2(-5, 0);
+            }
+        }
+        else
+        {
+            rigidBody.velocity = Vector2.down;
+        }
+    }
+}
