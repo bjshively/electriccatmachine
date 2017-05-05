a248425d6dc36e738221586cb4d0a71933a136ee
Bradley Shively <bshively@gmail.com>
2017-05-05 01:48:27 -0400

    Implement chasing cat facing logic and throwing in both directions

--- Electric Cat Machine/Assets/Scripts/Laser.cs
+++ Electric Cat Machine/Assets/Scripts/Laser.cs
@@ -7,7 +7,7 @@ public class Laser : MonoBehaviour
     private LineRenderer lineRenderer;
     private Vector3 laserPoint;
     private Vector3 mouseWorld;
-
+    private int facing;
     private GameObject babyNinja;
     //    private
     // Use this for initialization
@@ -22,20 +22,26 @@ public class Laser : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
+        mouseWorld = Input.mousePosition;
+        mouseWorld.z = 10.0f;
+        mouseWorld = Camera.main.ScreenToWorldPoint(mouseWorld);
+
+        facing = 1;
+        if (this.transform.position.x > mouseWorld.x)
+        {
+            facing = -1;
+        }
+
+        // TODO: Freeze player movement when laser pointer is active
         if (Input.GetMouseButtonDown(0) && !babyNinja)
         {
-            babyNinja = (GameObject)Instantiate(Resources.Load("BabyNinja"));
-            babyNinja.transform.position = this.transform.position;
-            babyNinja.GetComponent <Rigidbody2D>().AddForce(new Vector2(200, 200));
+            ThrowCat("LaserFollowingCat");
         }
         if (Input.GetMouseButton(0))
         {
             lineRenderer.enabled = true;
             // Translate mouse position to world position
-            mouseWorld = Input.mousePosition;
-            mouseWorld.z = 10.0f;
-            mouseWorld = Camera.main.ScreenToWorldPoint(mouseWorld);
-//        Vector3[] lineCoordinates = [this.transform, Input.mousePosition];
+
             if (Input.GetMouseButton(0))
             {
                 lineRenderer.SetPositions(new Vector3[]{ this.transform.position, mouseWorld });
@@ -46,6 +52,17 @@ public class Laser : MonoBehaviour
             lineRenderer.enabled = false;
             Destroy(babyNinja);
         }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ThrowCat("RunningCat");
+        }
+    }
+
+    public void ThrowCat(string catType)
+    {
+        babyNinja = (GameObject)Instantiate(Resources.Load(catType));
+        babyNinja.transform.position = this.transform.position;
+        babyNinja.GetComponent <Rigidbody2D>().AddForce(new Vector2(200 * facing, 200));
     }
 
 }
