ed038fcede0254ae56e2d0ef31bf358fcce52853
Bradley Shively <bshively@gmail.com>
2017-05-05 14:35:26 -0400

    Fix bugs in laser following cat

--- Electric Cat Machine/Assets/Scripts/LaserFollowingCat.cs
+++ Electric Cat Machine/Assets/Scripts/LaserFollowingCat.cs
@@ -6,15 +6,29 @@ public class LaserFollowingCat : Cat
 {
     protected override void Move()
     {
-        Flip();
-        if (!Input.GetMouseButton(0))
+        // As long as the cat is active, it should look at the mouse pointer
+        if (alive)
         {
-            alive = false;
+            Flip();
         }
 
-        if (IsGrounded())
+        // When mouse button is let off, deactive the cat
+        // This cat begins ignoring the mouse and runs off in the direction it is currently headed
+        if (Input.GetMouseButtonUp(0))
         {
-            
+            if (!IsGrounded())
+            {
+                base.Kill();
+            }
+            else
+            {
+                Kill();
+            }
+        }
+
+        // Cat mouse-follow behavior
+        else if (IsGrounded() && alive)
+        {
             float xpos = this.transform.position.x;
             if (mouseWorld.x > xpos)
             {
@@ -25,5 +39,12 @@ public class LaserFollowingCat : Cat
                 rigidBody.velocity = new Vector2(-5, rigidBody.velocity.y);
             }
         }
+    }
+
+    protected override void Kill()
+    {
+        Destroy(gameObject, 5);
+        rigidBody.velocity = new Vector2(10, rigidBody.velocity.y);
+        alive = false;
     }
 }
