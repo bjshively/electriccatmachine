5de3354f654a81aca9b7cbfb69dde08550bed66a
Eric Mofield <ericmofield@gmail.com>
2017-05-13 00:53:30 -0400

    Add TODO CS script.

---
+++ Electric Cat Machine/Assets/Scripts/_TODO.cs
@@ -0,0 +1,38 @@
+﻿/*
+
+
+// CATS
+GrayCat has a single frame for each pose. Not sure how to assign those frames to Animations yet.
+TODO: Add script features like Player's to allow falling animation, etc.
+TODO: Always eject a cat with the jumping sprite.
+
+
+
+// CRATES
+TODO: Wood boxes should break when they fall.
+The gray tiles in the level are supposed to be "pipes."
+TODO: The crate in that area should be assigned to the pipe.
+TODO: The pipe should eject a new crate whenever that assigned crate is lost. Pit or destoryed.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
